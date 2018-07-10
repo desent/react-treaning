a7ca2e92a1ad35c45272f759bccf8b2833197e93
desent <desent666@gmail.com>
2018-07-10 21:12:59 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+     })
+   ]
+};
